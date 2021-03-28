ebc5b4efc647267dd6c562e1ce49d4eae3dd64fc
MatteusFelippe <53882863+MatteusFelippe@users.noreply.github.com>
2021-03-28 00:54:05 -0300

    Update Quiz4Bean.java

--- src/main/java/io/github/annalauton/jakartaee8/atividadeavaliativa/Quiz4Bean.java
+++ src/main/java/io/github/annalauton/jakartaee8/atividadeavaliativa/Quiz4Bean.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package io.github.annalauton.jakartaee8.atividadeavaliativa;
 
 import javax.inject.Named;
